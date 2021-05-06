183e0213af04c49e457ae85e58fe07433298f48e
Juan Cruz Romero <ben12juancruz@gmail.com>
2021-05-06 09:10:58 +0200

    first commit

---
+++ src/paquete1/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package paquete1;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
